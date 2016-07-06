8224efca1eb52afab71ba92127e8fa2d85e50543
Michael Oberst <michael.k.oberst@gmail.com>
2016-07-05 20:33:55 -0700

    Change to correct version of pip

--- load-tensorflow.sh
+++ load-tensorflow.sh
@@ -8,4 +8,4 @@ source activate tensorflow
 TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp35-cp35m-linux_x86_64.whl
 
 # Install using pip 
-pip3 install --upgrade $TF_BINARY_URL
+pip install --upgrade $TF_BINARY_URL
