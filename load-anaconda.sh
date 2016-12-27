09ae0b28d3ee69b1d34f1823474ad95bc8b5f261
Michael Oberst <michael@clarifyhealth.com>
2016-12-27 20:15:30 +0000

    Make generic filepath to load anaconda

--- load-anaconda.sh
+++ load-anaconda.sh
@@ -3,5 +3,5 @@
 cd ~
 wget http://repo.continuum.io/archive/Anaconda3-4.1.0-Linux-x86_64.sh
 bash Anaconda3-4.1.0-Linux-x86_64.sh
-echo 'PATH="/home/ubuntu/anaconda3/bin:$PATH"' >> .bashrc
+echo 'PATH="$HOME/anaconda3/bin:$PATH"' >> .bashrc
 source .bashrc
