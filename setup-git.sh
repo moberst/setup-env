b2d8acfbbc4dd9f583f05d9d507a27069ec02d55
Michael Oberst <michael.k.oberst@gmail.com>
2017-02-19 05:14:20 +0000

    Change git setup to default username/email and editor to vim

--- setup-git.sh
+++ setup-git.sh
@@ -1,4 +1,11 @@
+git config --global user.name "Michael Oberst"
+git config --global user.email michael.k.oberst@gmail.com
+
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
+
+git config --global core.editor vim
+
+
