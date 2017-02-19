d9d5b0ee5fc7d7eeb057603dd224e4e3ab674e5e
Ubuntu <ubuntu@ip-172-31-49-54.ec2.internal>
2017-02-19 05:09:54 +0000

    Update tensorflow install to 1.0.0

--- load-tensorflow.sh
+++ load-tensorflow.sh
@@ -5,7 +5,8 @@ conda create -n tensorflow python=3.5
 source activate tensorflow
 
 # Set address for (non-GPU) version of tensorflow
-TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp35-cp35m-linux_x86_64.whl
+TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-cp35-cp35m-linux_x86_64.whl
 
 # Install using pip 
-pip install --upgrade $TF_BINARY_URL
+pip install --ignore-installed --upgrade $TF_BINARY_URL
+
