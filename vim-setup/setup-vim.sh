3d0d4d5fb3ecfc02aa9e833ae2cc695f8a59ebe5
Michael Oberst <michael.k.oberst@gmail.com>
2017-04-19 22:30:16 -0700

    Add setup scripts for VIM

---
+++ vim-setup/setup-vim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download vundle package manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# setup .vimrc with nerdtree
+cp example_vimrc ~/.vimrc
