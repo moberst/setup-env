8a3a5bdd230d5a6975bacbdeabdfdab4756840ba
Michael Oberst <michael.k.oberst@gmail.com>
2017-06-02 11:53:54 -0700

    Restructure git setup and configuration

---
+++ git/configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set up git configuations
+git config --global user.name "Michael Oberst"
+git config --global user.email michael.k.oberst@gmail.com
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global core.editor vim
+git config --global push.default simple
+git config --global credential.helper cache
--- git/setup.sh
+++ git/setup.sh
@@ -3,15 +3,4 @@
 # Install git
 sudo apt-get install git-all
 
-# Set up git configuations
-# Alternatively, use the my_gitconfig file, and copy to 
-# $HOME/.git/.gitconfig
-git config --global user.name "Michael Oberst"
-git config --global user.email michael.k.oberst@gmail.com
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global core.editor vim
-git config --global push.default simple
-git config --global credential.helper cache
+bash configure.sh
