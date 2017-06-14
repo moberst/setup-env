2f8c3ba6ca8eb8631395cdde9236a5b0e4b05ae4
Michael Oberst <michael.k.oberst@gmail.com>
2017-06-14 09:32:10 -0700

    Add step to configure.sh to update packages

--- vim/vim8/configure.sh
+++ vim/vim8/configure.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+VIM_DIR="$HOME/.vim"
+
 # setup .vimrc
 cp vimrc ~/.vimrc
 
+# Update plugins
+sudo cp install_pack.sh $VIM_DIR/pack/install_pack.sh
+cd $VIM_DIR/pack
+sudo bash install_pack.sh
+
+sudo vim -c "helptags ALL"
