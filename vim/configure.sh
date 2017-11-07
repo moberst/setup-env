671363381f59324792f3607bb276514e55706c90
Michael Oberst <michael.k.oberst@gmail.com>
2017-11-06 23:06:03 -0500

    Remove sudo from vim config script

--- vim/configure.sh
+++ vim/configure.sh
@@ -6,8 +6,8 @@ VIM_DIR="$HOME/.vim"
 cp vimrc ~/.vimrc
 
 # Update plugins
-sudo cp install_pack.sh $VIM_DIR/pack/install_pack.sh
+cp install_pack.sh $VIM_DIR/pack/install_pack.sh
 cd $VIM_DIR/pack
-sudo bash install_pack.sh
+bash install_pack.sh
 
-sudo vim -c "helptags ALL"
+vim -c "helptags ALL"
