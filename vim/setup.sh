4f64b30f98a9476d691ed8b9920c92fc50b51de6
Michael Oberst <michael.k.oberst@gmail.com>
2017-12-18 13:50:23 -0500

    Remove sudo from vim setup

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev 
 
@@ -10,8 +10,8 @@ SETUP_DIR="$HOME/repos/setup-env/vim/"
 VIM_DIR="$HOME/.vim"
 
 # Delete vim if it already exists
-sudo rm -rf "$VIM_DIR"
-sudo apt remove vim
+rm -rf "$VIM_DIR"
+apt remove vim
 
 # Clone the source code into the directory
 if [ ! -d "$VIM_DIR" ] ; then
@@ -24,12 +24,12 @@ if [ ! -d "$VIM_DIR" ] ; then
             --with-python3-config-dir=$PYTHON_CONFIG_DIR \
             --enable-cscope 
 	make
-	sudo make install
+	make install
 fi
 
-sudo mkdir "$VIM_DIR/pack"
-sudo cp $SETUP_DIR/install_pack.sh $VIM_DIR/pack/install_pack.sh
+mkdir "$VIM_DIR/pack"
+cp $SETUP_DIR/install_pack.sh $VIM_DIR/pack/install_pack.sh
 cd $VIM_DIR/pack
-sudo bash install_pack.sh
+bash install_pack.sh
 
-sudo vim -c "helptags ALL"
+vim -c "helptags ALL"
