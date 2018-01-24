ae58fb31a18b5bad9fc45dfcd0a1e98a02ffae16
Michael Oberst <moberst@mit.edu>
2018-01-24 14:41:54 -0500

    Update vim setup script for python 3.6

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
+apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
 apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev 
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev python-dev
 
-PYTHON_CONFIG_DIR="/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu"
+apt-get install checkinstall
+
+PYTHON_CONFIG_DIR="/usr/lib/python2.7/config-x86_64-linux-gnu"
+PYTHON_3_CONFIG_DIR="/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu"
 
 SETUP_DIR="$HOME/repos/setup-env/vim/"
 VIM_DIR="$HOME/.vim"
 
 # Delete vim if it already exists
 rm -rf "$VIM_DIR"
+rm -rf /usr/local/share/vim /usr/bin/vim
 apt remove vim
 
 # Clone the source code into the directory
 if [ ! -d "$VIM_DIR" ] ; then
-	git clone https://github.com/vim/vim.git "$VIM_DIR"
-	cd "$VIM_DIR/src"
-	make distclean
-  ./configure --with-features=huge \
+    git clone https://github.com/vim/vim.git "$VIM_DIR"
+    cd "$VIM_DIR/src"
+    git pull && git fetch
+    make distclean
+    ./configure --with-features=huge \
             --enable-multibyte \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=$PYTHON_CONFIG_DIR \
-            --enable-cscope 
-	make
-	make install
+            --enable-pythoninterp=dynamic \
+            --with-python-config-dir="$PYTHON_CONFIG_DIR"\
+            --enable-python3interp \
+            --with-python3-config-dir="$PYTHON_3_CONFIG_DIR"\
+            --with-x \
+            --with-compiledby="moberst" \
+            --enable-fail-if-missing \
+            --enable-fontset \
+            --enable-largefile \
+            --disable-netbeans \
+            --enable-gui=auto \
+            --enable-cscope
+    make
+    make install
 fi
 
 mkdir "$VIM_DIR/pack"
