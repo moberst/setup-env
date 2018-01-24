2a4f1c995292bd8500a051134e13de918fb6087e
Michael Oberst <moberst@mit.edu>
2018-01-24 15:20:58 -0500

    Dont bother renaming base.bib, and leave main file as main.tex

--- tex/create_base.sh
+++ tex/create_base.sh
@@ -17,18 +17,13 @@ mkdir $NAME/sections
 echo "Moving files"
 # Move over the files
 cp $SCRIPT_HOME/header.tex $SCRIPT_HOME/introduction.tex \
-   $SCRIPT_HOME/natbib.sty $SCRIPT_HOME/jmb.bst $SCRIPT_HOME/base.tex \
+   $SCRIPT_HOME/natbib.sty $SCRIPT_HOME/jmb.bst $SCRIPT_HOME/main.tex \
    $SCRIPT_HOME/base.bib $NAME
 
 # No Makefile, just use vimtex
 
 # Change file names
-mv $NAME/base.tex $NAME/$NAME.tex
-mv $NAME/base.bib $NAME/$NAME.bib
 mv $NAME/introduction.tex $NAME/sections/
-
-sed -i '.bak' 's/\bibliography{base}/\bibliography{'$NAME'}/g' $NAME/$NAME.tex
-rm $NAME/$NAME.tex.bak
 
 echo "Initializing Git Repository"
 # Initialize git repository
