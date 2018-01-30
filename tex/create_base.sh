84476eac447e6726e7d67c6ab2d05aaa52b71964
Michael Oberst <moberst@mit.edu>
2018-01-30 11:59:01 -0500

    Add basic class notes version

--- tex/create_base.sh
+++ tex/create_base.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
 # Enable this to be run from another directory (where the project lives)
 SCRIPT_HOME="$HOME/repos/setup-env/tex"
 NAME=$1
+FULL_SETUP=0
+
+# Parse arguments
+while getopts "h?f" opt; do
+	case "$opt" in
+	h|\?)
+		show_help
+		exit 0
+		;;
+	f)
+		FULL_SETUP=1
+		;;
+	esac
+done
 
 # Check if directory already exists
 if [ -d "$NAME" ]; then
@@ -11,21 +29,29 @@ fi
 echo "Creating directory structure"
 # Create directory structure
 mkdir $NAME
-mkdir $NAME/figs
-mkdir $NAME/sections
 
-echo "Moving files"
-# Move over the files
-cp $SCRIPT_HOME/header.tex $SCRIPT_HOME/introduction.tex \
-   $SCRIPT_HOME/natbib.sty $SCRIPT_HOME/jmb.bst $SCRIPT_HOME/main.tex \
-   $SCRIPT_HOME/base.bib $NAME
+if [ $FULL_SETUP -eq 1 ]; then
+  mkdir $NAME/figs
+  mkdir $NAME/sections
 
-# No Makefile, just use vimtex
+  echo "Moving files"
+  # Move over the files
+  cp $SCRIPT_HOME/header.tex $SCRIPT_HOME/introduction.tex \
+     $SCRIPT_HOME/natbib.sty $SCRIPT_HOME/jmb.bst $SCRIPT_HOME/main.tex \
+     $SCRIPT_HOME/base.bib $NAME
 
-# Change file names
-mv $NAME/introduction.tex $NAME/sections/
+  # No Makefile, just use vimtex
 
-echo "Initializing Git Repository"
-# Initialize git repository
-cd $NAME
-git init 
+  # Change file names
+  mv $NAME/introduction.tex $NAME/sections/
+
+  echo "Initializing Git Repository"
+  # Initialize git repository
+  cd $NAME
+  git init 
+else
+  echo "Moving basic files"
+  mkdir $NAME/tex
+  cp $SCRIPT_HOME/header.tex $NAME/tex/header.tex
+  cp $SCRIPT_HOME/basic.tex $NAME/main.tex
+fi
