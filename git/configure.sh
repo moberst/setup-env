0c9e731715e694f9067ca45b9aa8f8d41fb5e813
Michael Oberst <michael.k.oberst@gmail.com>
2018-06-12 11:49:35 -0400

    Add cool git log commands

--- git/configure.sh
+++ git/configure.sh
@@ -4,7 +4,7 @@
 git config --global init.templatedir '~/.git_template'
 mkdir -p ~/.git_template
 if [ -d ~/.git_template/hooks ]; then
-  rm ~/.git_template/hooks -rf 
+    rm ~/.git_template/hooks -rf
 fi
 cp hooks/ ~/.git_template/hooks -r
 git config --global alias.ctags '!.git/hooks/ctags'
@@ -19,3 +19,5 @@ git config --global alias.br branch
 git config --global core.editor vim
 git config --global push.default simple
 git config --global credential.helper cache
+
+cat gitconfig >> $HOME/.gitconfig
