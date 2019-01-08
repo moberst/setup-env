a0690228d05cb07b539c170d696b0b1188d542e4
Michael Oberst <moberst@mit.edu>
2019-01-08 00:42:34 -0500

    Update bashrc, dbeaver, oni config

--- dbeaver/setup.sh
+++ dbeaver/setup.sh
@@ -1,5 +1,4 @@
-wget -P $HOME "https://github.com/serge-rider/dbeaver/releases/download/4.0.5/dbeaver-ce_4.0.5_amd64.deb"
-
-sudo dpkg -i "$HOME/dbeaver-ce_4.0.5_amd64.deb"
+#wget -P $HOME "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb"
+#sudo dpkg -i "$HOME/dbeaver-ce_latest_amd64.deb"
 
 sudo apt-get install -f
--- oni/config.tsx
+++ oni/config.tsx
@@ -1,27 +1,17 @@
-
-import * as React from "react"
-import * as Oni from "oni-api"
-
-export const activate = (oni: Oni.Plugin.Api) => {
-  oni.input.unbind("<c-g>") // make C-g work as expected in vim
-  oni.input.unbind("<c-v>") // make C-v work as expected in vim
-  oni.input.bind("<s-c-g>", () => oni.commands.executeCommand("sneak.show")) // You can rebind Oni's behaviour to a new keybinding
-}
-
-export const configuration = {
-    activate,
+module.exports = {
     "oni.hideMenu"             : "hidden", // Hide top bar menu
     "oni.loadInitVim"          : true, // Load user's init.vim
     "oni.useDefaultConfig"     : false, // Do not load Oni's init.vim
     "ui.colorscheme"           : "n/a", // Load init.vim colorscheme
     "autoClosingPairs.enabled" : false, // disable autoclosing pairs
-    "commandline.mode"         : false, // Do not override commandline UI
-    "wildmenu.mode"            : false, // Do not override wildmenu UI,
+    "commandline.mode"         : true, 
+    "wildmenu.mode"            : true, 
     "tabs.mode"                : "native", // Use vim's tabline
     "sidebar.enabled"          : false, // sidebar ui is gone
     "sidebar.default.open"     : false, // the side bar collapse 
     "learning.enabled"         : false, // Turn off learning pane
     "achievements.enabled"     : false, // Turn off achievements tracking / UX
     "editor.typingPrediction"  : false, // Wait for vim's confirmed typed chars
-    // "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
+    "editor.textMateHighlighting.enabled" : true, 
+    "editor.fontSize": "16px",
 }
