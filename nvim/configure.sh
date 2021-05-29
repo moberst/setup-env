2e91d858978a08aa83795245a093c1c115bbeae6
Michael Oberst <moberst@mit.edu>
2021-05-29 18:24:52 -0400

    Update

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# cat init.vim google.vim coc.vim > ~/.config/nvim/init.vim
-cp init.vim ~/.config/nvim/init.vim
+# Version that includes nvim.coc
+cat init_coc.vim google.vim coc.vim > ~/.config/nvim/init.vim
+cp coc-settings.json ~/.config/nvim/coc-settings.json
+
+# Basic version without nvim.coc
+# cp init.vim ~/.config/nvim/init.vim
+
 if [ ! -f /usr/share/nvim/runtime/autoload/plug.vim ]; then
   sudo cp plug.vim /usr/share/nvim/runtime/autoload/plug.vim
 fi
--- nvim/dependencies.sh
+++ nvim/dependencies.sh
@@ -1,1 +1,4 @@
 pip install python-language-server
+
+# If Node is installed
+npm install -g neovim
