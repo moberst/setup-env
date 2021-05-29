ce0e09e56eec3bce0a80d5cbca0d9b5cde06fa31
Michael Oberst <moberst@mit.edu>
2021-05-29 12:42:25 -0400

    Split vimconfig into different files

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# cat init.vim google.vim coc.vim > ~/.config/nvim/init.vim
 cp init.vim ~/.config/nvim/init.vim
 if [ ! -f /usr/share/nvim/runtime/autoload/plug.vim ]; then
   sudo cp plug.vim /usr/share/nvim/runtime/autoload/plug.vim
