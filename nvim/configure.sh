6f9957fbbf89e0bd2a46aa14365f11498cba9b4e
Michael Oberst <moberst@mit.edu>
2021-06-03 20:07:11 -0400

    Update configuration and dependencies
    Separate out init.vim for nightly versus stable

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Version that includes nvim.coc
-cat init_coc.vim google.vim coc.vim > ~/.config/nvim/init.vim
-cp coc-settings.json ~/.config/nvim/coc-settings.json
+# cat init_coc.vim google.vim coc.vim > ~/.config/nvim/init.vim
+# cp coc-settings.json ~/.config/nvim/coc-settings.json
 
 # Basic version without nvim.coc
-# cp init.vim ~/.config/nvim/init.vim
+cp init.vim stable.vim nightly.vim ~/.config/nvim/
 
 if [ ! -f /usr/share/nvim/runtime/autoload/plug.vim ]; then
   sudo cp plug.vim /usr/share/nvim/runtime/autoload/plug.vim
--- nvim/dependencies.sh
+++ nvim/dependencies.sh
@@ -2,3 +2,7 @@ pip install python-language-server
 
 # If Node is installed
 npm install -g neovim
+
+# For YouCompleteMe
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs default-jdk npm
