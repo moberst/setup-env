1becc87713c6e69c809fe9859aa1869165951906
Michael Oberst <moberst@mit.edu>
2021-06-03 20:09:58 -0400

    Update git config to use nvim

--- git/configure.sh
+++ git/configure.sh
@@ -16,7 +16,7 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global core.editor vim
+git config --global core.editor nvim
 git config --global push.default simple
 git config --global credential.helper cache
 git config --global Core.fileMode false
