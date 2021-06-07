a9b2419a036b23e0b63577f62f0ae69d21bb8d35
Michael Oberst <michael.k.oberst@gmail.com>
2021-06-06 20:14:36 -0400

    Comments

--- linters/configure.sh
+++ linters/configure.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# Note: These are used for ALE, linting in nvim.coc should be disabled
-
 # Make pylintrc the fallback option
 sudo cp config/pylint_config /etc/pylintrc
 
-# Defaults for yapf
+# Defaults for other linters
 cp config/yapf_config ~/.config/yapf/style
 cp config/mypy_config ~/.config/mypy/config
 cp config/flake_config ~/.config/flake8
