638a0433b39ae39022f9d9e4d31834106be7bf33
Michael Oberst <michael.k.oberst@gmail.com>
2022-01-10 23:56:44 -0500

    Add diff-so-fancy to git config

--- git/configure.sh
+++ git/configure.sh
@@ -20,5 +20,7 @@ git config --global core.editor nvim
 git config --global push.default simple
 git config --global credential.helper cache
 git config --global Core.fileMode false
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
 
 cat gitconfig >> $HOME/.gitconfig
