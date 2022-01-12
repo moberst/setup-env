eee76ca3a89b7e90a62b20040883d6a4fa64b531
Michael Oberst <michael.k.oberst@gmail.com>
2022-01-12 00:48:23 -0500

    Remove ctags from git, and streamline config

--- git/configure.sh
+++ git/configure.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Set up ctags template (for Vim)
-git config --global init.templatedir '~/.git_template'
-mkdir -p ~/.git_template
-if [ -d ~/.git_template/hooks ]; then
-    rm ~/.git_template/hooks -rf
-fi
-cp hooks/ ~/.git_template/hooks -r
-git config --global alias.ctags '!.git/hooks/ctags'
-
 # Set up git configuations
 git config --global user.name "Michael Oberst"
 git config --global user.email michael.k.oberst@gmail.com
@@ -20,5 +11,8 @@ git config --global core.editor nvim
 git config --global push.default simple
 git config --global credential.helper cache
 git config --global Core.fileMode false
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global pull.rebase false
+
+# Only run these once diff-so-fancy is installed
+# git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+# git config --global interactive.diffFilter "diff-so-fancy --patch"
