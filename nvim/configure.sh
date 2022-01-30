1275dbadafd38e1e0d3cf3b4ba39457739aed84b
Michael Oberst <michael.k.oberst@gmail.com>
2022-01-30 14:55:05 -0500

    Update VimConfig

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -2,9 +2,3 @@
 
 cd ./config
 cp init.vim config.vim google.vim ~/.config/nvim/
-
-RUNTIME_PATH=$HOME/.neovim/share/nvim/runtime
-
-if [ ! -f $RUNTIME_PATH/autoload/plug.vim ]; then 
-  sudo cp plug.vim $RUNTIME_PATH/autoload/plug.vim
-fi
