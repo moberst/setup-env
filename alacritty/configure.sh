51f945e62de9f310e7bbe39981f8eced236d13c4
Michael Oberst <michael.k.oberst@gmail.com>
2022-02-22 08:38:15 -0500

    Update alacritty themes

--- alacritty/configure.sh
+++ alacritty/configure.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/.config/alacritty
-cat ./alacritty.yml ./themes/solarized_dark.yml > ~/.config/alacritty/alacritty.yml
+cat ./alacritty.yml ./themes/tokyo_night_storm.yml > ~/.config/alacritty/alacritty.yml
