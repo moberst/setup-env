29cfe15c43a149f27743596ded59a01ff1efe201
Michael Oberst <michael.k.oberst@gmail.com>
2022-06-15 20:55:12 -0400

    Update linter config

--- linters/configure.sh
+++ linters/configure.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Make pylintrc the fallback option
-# sudo cp config/pylint_config /etc/pylintrc
-
-# Defaults for other linters
+cp config/pylint_config ~/.config/pylintrc
 cp config/yapf_config ~/.config/yapf/style
 cp config/mypy_config ~/.config/mypy/config
 cp config/flake_config ~/.config/flake8
+cp config/gitlint ~/.config/gitlint
