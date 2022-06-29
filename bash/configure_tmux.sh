88a0fad047841c267c7ad82a44e68a8ad7fea8c9
Michael Oberst <michael.k.oberst@gmail.com>
2022-06-28 22:28:07 -0400

    feat(tmux): Enable plugins and tmux-yank

--- bash/configure_tmux.sh
+++ bash/configure_tmux.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  mkdir -p ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 cat tmux_tokyonight.conf tmux.conf > $HOME/.tmux.conf
