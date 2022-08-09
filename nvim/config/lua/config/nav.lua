8e2c0a5893aa8eec4b9954dbc54a719351a0888b
Michael Oberst <michael.k.oberst@gmail.com>
2022-08-09 12:49:14 -0400

    feat(nvim): Better autosession config

--- nvim/config/lua/config/nav.lua
+++ nvim/config/lua/config/nav.lua
@@ -1,6 +1,16 @@
+function _G.close_all_floating_wins()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= '' then
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+end
+
 require('auto-session').setup({
       log_level = 'info',
-      auto_session_suppress_dirs = {'~/'}
+      auto_session_suppress_dirs = {'~/', '~/Dropbox/research/wiki'},
+      pre_save_cmds = { _G.close_all_floating_wins },
     })
 
 require('session-lens').setup()
