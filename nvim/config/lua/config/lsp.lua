f4904444ceaaca1e58eef3d2119220a16bb859fc
Michael Oberst <michael.k.oberst@gmail.com>
2023-01-16 00:21:39 -0500

    feat(nvim): Add <CR> as incremental selector in normal
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/config/lua/config/lsp.lua
+++ nvim/config/lua/config/lsp.lua
@@ -103,7 +103,16 @@ require('nvim-treesitter.configs').setup({
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
-  }
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      scope_incremental = "<S-CR>",
+      node_decremental = "<BS>",
+    },
+  },
 })
 
 require("lsp_lines").setup()
