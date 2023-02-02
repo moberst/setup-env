1ed2ed557428efcc9d856a8b9a381f1745d747bb
Michael Oberst <michael.k.oberst@gmail.com>
2023-02-01 22:11:06 -0500

    feat(git): Refactor into file and copy
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- git/configure.sh
+++ git/configure.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-# Set up git configuations
-git config --global user.name "Michael Oberst"
-git config --global user.email michael.k.oberst@gmail.com
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global core.editor nvim
-git config --global push.default simple
-git config --global credential.helper cache
-git config --global Core.fileMode false
-git config --global pull.rebase false
-git config --global init.defaultBranch main 
-
-# Only run these once diff-so-fancy is installed
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
+cp gitconfig.yml ~/.gitconfig
+cp lazygit_config.yml ~/.config/lazygit/config.yml
