f8da4e069878a170b7e7da39ab0eaacfb979ee45
Michael Oberst <michael.k.oberst@gmail.com>
2024-07-30 09:21:34 -0400

    feat(python): Add config
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

---
+++ python/configure.sh
@@ -0,0 +1,5 @@
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
