ac243f8ba033f0a1c62dc2138cd8f7842aa766e1
Michael Oberst <michael.k.oberst@gmail.com>
2024-07-30 09:21:00 -0400

    feat(tmux): Remove unused layouts
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- tmux/configure_tmux.sh
+++ tmux/configure_tmux.sh
@@ -7,5 +7,3 @@ fi
 
 # cat tmux_tokyonight.conf tmux.conf > $HOME/.tmux.conf
 cat ./themes/tmux_catppuccin.conf tmux.conf > $HOME/.tmux.conf
-cp ./layouts/driver.yml $HOME/.config/tmuxinator/driver.yml
-cp ./layouts/driver-small.yml $HOME/.config/tmuxinator/driver-small.yml
