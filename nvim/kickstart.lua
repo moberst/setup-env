8a76596fc0df3f6923ff62ee7aeb23c855e62066
Michael Oberst <michael.k.oberst@gmail.com>
2025-01-19 14:20:51 -0500

    feat(nvim): Add yazi
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/kickstart.lua
+++ nvim/kickstart.lua
@@ -77,6 +77,41 @@ vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
+	---@type LazySpec
+	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			{
+				"<leader>yh",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open [y]azi [h]ere: at current file",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>yc",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open [y]azi in the [c]urrent directory",
+			},
+			{
+				-- NOTE: this requires a version of yazi that includes
+				-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+				"<c-up>",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		},
+		---@type YaziConfig
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = false,
+			keymaps = {
+				show_help = "<f1>",
+			},
+		},
+	},
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
