89a930be5056d8ed765dc7cb19a63091dc9b14de
Michael Oberst <michael.k.oberst@gmail.com>
2025-01-18 19:48:09 -0500

    feat(nvim): Add back toggleterm
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/kickstart.lua
+++ nvim/kickstart.lua
@@ -82,7 +82,7 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -134,9 +134,43 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  To update plugins you can run
 --    :Lazy update
+
+-- Only used for generic terminals, not lazygit
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			size = function(term)
+				if term.direction == "horizontal" then
+					return 20
+				elseif term.direction == "vertical" then
+					return vim.o.columns * 0.4
+				end
+			end,
+			open_mapping = [[<c-\>]],
+			start_in_insert = false,
+			insert_mappings = true,
+			terminal_mappings = true,
+			persist_size = true,
+			direction = "horizontal",
+			on_open = function(term)
+				set_terminal_keymaps()
+			end, -- function to run when the terminal opens
+			close_on_exit = true,
+			shade_terminals = true,
+		},
+	},
 	{ "stevearc/dressing.nvim", opts = {} },
 	{
 		"folke/noice.nvim",
