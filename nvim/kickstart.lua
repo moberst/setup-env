119fa426da119c156f90393fbfe0dbf7720c0926
Michael Oberst <michael.k.oberst@gmail.com>
2025-01-18 20:33:41 -0500

    feat(nvim): Fixup gitsigns
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/kickstart.lua
+++ nvim/kickstart.lua
@@ -320,17 +320,40 @@ require("lazy").setup({
 	-- See `:help gitsigns` to understand what the configuration keys do
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local function map(mode, lhs, rhs, opts)
+						opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
+						vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+					end
 
+					-- Navigation
+					map("n", "]h", "<cmd>Gitsigns next_hunk<CR>")
+					map("n", "[h", "<cmd>Gitsigns prev_hunk<CR>")
+
+					-- Actions
+					map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+					map("v", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+					map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
+					map("v", "<leader>hr", ":Gitsigns reset_hunk<CR>")
+					map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>")
+					map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
+					map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>")
+					map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
+					map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+					map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+					map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
+					map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+					map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
+
+					-- Text object
+					map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+					map("x", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
 	-- This is often very useful to both group configuration, as well as handle
