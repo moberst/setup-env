f93ff5703b44fe3a742367b47e1aafe9932a5e2e
Michael Oberst <michael.k.oberst@gmail.com>
2025-01-29 09:50:27 -0500

    style(nvim): Formatting
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/config/lua/config/lsp.lua
+++ nvim/config/lua/config/lsp.lua
@@ -1,125 +1,122 @@
 local function on_attach(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 
-  if client.supports_method("textDocument/formatting") then
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-      })
-  end 
-
+	if client.supports_method("textDocument/formatting") then
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end,
+		})
+	end
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { 
-    'ruff',
-    'pylsp',
-  },
+	ensure_installed = {
+		"ruff",
+		"pylsp",
+	},
 })
 
 local lspconfig = require("lspconfig")
 lspconfig.ruff.setup({
-    cmd_env = { RUFF_TRACE = "messages" },
-    init_options = {
-      logLevel = "debug",
-  },
-    on_attach = on_attach,
-    capabilities = capabilities,
+	cmd_env = { RUFF_TRACE = "messages" },
+	init_options = {
+		logLevel = "debug",
+	},
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.pylsp.setup {
-  settings = {
-    -- See https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
-    pylsp = {
-      plugins = {
-        autopep8 = { enabled = false },
-        flake8 = { enabled = false },
-        pycodestyle = { enabled = false },
-        pydocstyle = { enabled = false },
-        pyflakes = { enabled = false },
-        pylint = { enabled = false },
-        yapf = { enabled = false },
-        }
-      }
-    }
-  }
+lspconfig.pylsp.setup({
+	settings = {
+		-- See https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+		pylsp = {
+			plugins = {
+				autopep8 = { enabled = false },
+				flake8 = { enabled = false },
+				pycodestyle = { enabled = false },
+				pydocstyle = { enabled = false },
+				pyflakes = { enabled = false },
+				pylint = { enabled = false },
+				yapf = { enabled = false },
+			},
+		},
+	},
+})
 require("mason-null-ls").setup({
-  handlers = {},
+	handlers = {},
 })
 
-require('null-ls').setup({
-  debug = true,
+require("null-ls").setup({
+	debug = true,
 })
 
-require('treesitter-context').setup()
-require('nvim-treesitter.configs').setup({
-  textobjects = {
-    select = {
-      enable = true,
+require("treesitter-context").setup()
+require("nvim-treesitter.configs").setup({
+	textobjects = {
+		select = {
+			enable = true,
 
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-      },
-    },
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<CR>",
-      node_incremental = "<CR>",
-      scope_incremental = "<S-CR>",
-      node_decremental = "<BS>",
-    },
-  },
-  indent = {
-    enable = true,
-    disable = { "yaml" },
-  },
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+			},
+		},
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			node_incremental = "<CR>",
+			scope_incremental = "<S-CR>",
+			node_decremental = "<BS>",
+		},
+	},
+	indent = {
+		enable = true,
+		disable = { "yaml" },
+	},
 })
 
 require("lsp_lines").setup()
 -- Off by default, but the command below will toggle between the two
 vim.diagnostic.config({ virtual_lines = false })
 -- Define command for toggling lsp_lines versus normal mode
-vim.api.nvim_create_user_command(
-  'ToggleLSPLines',
-  function(opts)
-    local new_text = not vim.diagnostic.config().virtual_text
-    local new_lines = not vim.diagnostic.config().virtual_lines
-    vim.diagnostic.config({ virtual_lines = new_lines})
-    vim.diagnostic.config({ virtual_text = new_text })
-  end,
-  {}
-)
+vim.api.nvim_create_user_command("ToggleLSPLines", function(opts)
+	local new_text = not vim.diagnostic.config().virtual_text
+	local new_lines = not vim.diagnostic.config().virtual_lines
+	vim.diagnostic.config({ virtual_lines = new_lines })
+	vim.diagnostic.config({ virtual_text = new_text })
+end, {})
