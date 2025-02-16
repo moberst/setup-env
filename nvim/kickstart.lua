6bd5a52ca335d093a17bd42a425e86361aa5a843
Michael Oberst <michael.k.oberst@gmail.com>
2025-02-16 16:30:24 -0500

    feat(nvim): Remove avante

--- nvim/kickstart.lua
+++ nvim/kickstart.lua
@@ -828,64 +828,6 @@ require("lazy").setup({
 		},
 	},
 	{
-		-- "moberst/avante.nvim",
-		-- branch = "dev",
-		dir = "~/repos/avante.nvim",
-		dev = true,
-		event = "VeryLazy",
-		priority = 0,
-		lazy = false,
-		config = function()
-			require("avante_lib").load()
-			require("avante").setup({
-				provider = "claude",
-				-- See https://github.com/yetone/avante.nvim/pull/1072
-				auto_suggestions_provider = "claude",
-				-- add any opts here
-			})
-		end,
-		build = "make BUILD_FROM_SOURCE=true",
-		dependencies = {
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			{
-				"zbirenbaum/copilot.lua",
-				config = function()
-					require("copilot").setup({})
-				end,
-			},
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
-	},
-	{
 		"vimwiki/vimwiki",
 		lazy = false,
 		init = function()
