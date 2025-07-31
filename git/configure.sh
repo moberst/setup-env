ce207277d135e328c68436b04faed21f1684e8bf
Michael Oberst <moberst@abridge.com>
2025-07-31 23:44:32 +0000

    refactor: Remove unnecessary git gode

--- git/configure.sh
+++ git/configure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p ~/.config/lazygit
 cp gitconfig.yml ~/.gitconfig
 cp lazygit_config.yml ~/.config/lazygit/config.yml
--- git/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install git
-sudo apt-get install git-all
-sudo apt-get install exuberant-ctags
-
-sudo bash configure.sh
