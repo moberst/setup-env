933f380554c8df84628630da1bcf9859d2ee9fb9
Michael Oberst <moberst@abridge.com>
2025-08-01 00:09:46 +0000

    refactor: Delete unused folders

--- zk/install.sh
+++
@@ -1,7 +0,0 @@
-VERSION='v0.12.0'
-FILE="zk-${VERSION}-linux-amd64.tar.gz"
-wget "https://github.com/mickael-menu/zk/releases/download/${VERSION}/${FILE}"
-tar -xvf $FILE
-chmod 755 zk
-mv zk /home/moberst/.local/bin
-rm $FILE
