47aa2dfe2355889a43a9496a642169aeac699136
Michael Oberst <michael.k.oberst@gmail.com>
2026-04-01 19:29:44 -0400

    feat(nvim): Minor update for 0.12 treesitter
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/plugins/treesitter.lua
+++ nvim/plugins/treesitter.lua
@@ -1,33 +1,23 @@
-return { -- Highlight, edit, and navigate code
+return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	opts = {
-		ensure_installed = {
+		install_dir = vim.fn.stdpath("data") .. "/site",
+	},
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
+		require("nvim-treesitter").install({
 			"bash",
-			"c",
 			"diff",
-			"html",
 			"lua",
 			"luadoc",
 			"markdown",
 			"markdown_inline",
-			"query",
+			"python",
 			"vim",
 			"vimdoc",
 			"yaml",
-		},
-		-- Autoinstall languages that are not installed
-		auto_install = false,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-			disable = { "markdown", "vimwiki" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
+		})
+	end,
 }
