9d3c5b6c464a28f0cdcc3cfdd773992fd970a5c3
Michael Oberst <michael.k.oberst@gmail.com>
2026-04-07 13:45:16 -0400

    feat(nvim): Disable vim-markdown for now
    vim-markdown was overriding gx for opening links in markdown files.  I
    believe I wanted it becuase it helps with math mode shortcuts, but not
    using that so much at the moment.
    
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/plugins/markdown-utils.lua
+++ nvim/plugins/markdown-utils.lua
@@ -6,14 +6,14 @@ return {
 		---@type render.md.UserConfig
 		opts = {},
 	},
-	{
-		"plasticboy/vim-markdown",
-		branch = "master",
-		require = { "godlygeek/tabular" },
-		config = function()
-			vim.g.vim_markdown_math = 1
-			vim.g.vim_markdown_auto_insert_bullets = 0
-			vim.g.tex_conceal = ""
-		end,
-	},
+	-- {
+	-- 	"plasticboy/vim-markdown",
+	-- 	branch = "master",
+	-- 	require = { "godlygeek/tabular" },
+	-- 	config = function()
+	-- 		vim.g.vim_markdown_math = 1
+	-- 		vim.g.vim_markdown_auto_insert_bullets = 0
+	-- 		vim.g.tex_conceal = ""
+	-- 	end,
+	-- },
 }
