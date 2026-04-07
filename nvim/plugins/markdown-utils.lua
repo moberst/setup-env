29b54c02396b6a75f72785ae4d7c4ba18df32918
Michael Oberst <michael.k.oberst@gmail.com>
2026-04-07 15:52:05 -0400

    feat(nvim): Update vim-markdown to use local version

--- nvim/plugins/markdown-utils.lua
+++ nvim/plugins/markdown-utils.lua
@@ -6,14 +6,16 @@ return {
 		---@type render.md.UserConfig
 		opts = {},
 	},
-	-- {
-	-- 	"plasticboy/vim-markdown",
-	-- 	branch = "master",
-	-- 	require = { "godlygeek/tabular" },
-	-- 	config = function()
-	-- 		vim.g.vim_markdown_math = 1
-	-- 		vim.g.vim_markdown_auto_insert_bullets = 0
-	-- 		vim.g.tex_conceal = ""
-	-- 	end,
-	-- },
+	{
+		"moberst/vim-markdown",
+		dev = true,
+		branch = "master",
+		require = { "godlygeek/tabular" },
+		config = function()
+			vim.g.vim_markdown_math = 1
+			vim.g.vim_markdown_no_default_key_mapping = 1
+			vim.g.vim_markdown_auto_insert_bullets = 0
+			vim.g.tex_conceal = ""
+		end,
+	},
 }
