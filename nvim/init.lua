8c52f6471307632e005e220893dcaca884d0ac54
Michael Oberst <michael.k.oberst@gmail.com>
2026-04-18 11:18:36 -0400

    feat(nvim): Add undotree
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,5 +36,8 @@ require("lazy").setup({
 		},
 	},
 })
+
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set("n", "<leader>u", require("undotree").open)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
