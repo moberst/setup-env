8a1cb29f9ac6d9a9490604d8655b78b3b263eb6d
Michael Oberst <michael.k.oberst@gmail.com>
2026-05-25 19:38:14 -0400

    setup: Update install_rust.sh

--- install_scripts/install_rust.sh
+++ install_scripts/install_rust.sh
@@ -1,7 +1,9 @@
 curl https://sh.rustup.rs -sSf | sh
-
 source $HOME/.cargo/env
 
-cargo install zoxide
-cargo install eza
-cargo install starship --locked
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+cargo binstall zoxide
+cargo binstall eza
+cargo binstall starship
+cargo binstall ripgrep
